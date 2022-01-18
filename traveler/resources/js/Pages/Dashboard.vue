2e39f976f085f5337e5ed0d3a9e4e4371a8389dc
Aleksandre Galogre-Baramidze <74540578+AleksandreGalogreBaramidze@users.noreply.github.com>
2022-01-18 13:09:38 +0400

    1.0.0

---
+++ traveler/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,58 @@
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <Select2
+                            v-model="myValue"
+                            :options="myOptions"
+                            :settings="{ width: '100%' }"
+                            @change="myChangeEvent($event)"
+                            @select="mySelectEvent($event)" />
+                        <h4>Value: {{ myValue }}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import { Head } from '@inertiajs/inertia-vue3';
+import Select2 from 'vue3-select2-component';
+
+export default {
+    components: {
+        BreezeAuthenticatedLayout,
+        Head,
+        Select2
+    },
+    data(){
+        return {
+            myValue: '',
+            myOptions: ['op1', 'op2', 'op3']
+        }
+    },
+    methods: {
+        myChangeEvent(val){
+            console.log(val);
+        },
+        mySelectEvent({id, text}){
+            console.log({id, text})
+        }
+    },
+    created() {
+
+    }
+}
+</script>
